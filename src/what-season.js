aab7df6c5236d335de230c6af85314cd5fecaff9
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-03-18 18:44:49 +0200

    fix: add corretly handles argument absence in what-seasons.js

--- src/what-season.js
+++ src/what-season.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
   if (!date)  {
-    return "Unable todetermine the time of year!";
+    return "Unable to determine the time of year!";
   }
   try {
     const testdate = new Date(date);
