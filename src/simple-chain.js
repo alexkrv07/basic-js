0992fe7ca829b34938e691ffc0cdbe95acc732a7
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-03-19 18:28:16 +0200

    feat: implement simple-chin.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  start: "(",
+  end: ")",
+  link: "~~",
+  chain: [],
+  whitespace: " ",
+
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length; 
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    value = (!arguments.length) ? this.whitespace : this.whitespace + value + this.whitespace;
+    this.chain.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    if (!Number.isInteger(position) || position < 0 || position > (this.chain.length - 1)) {
+      throw new Error("Invalid position");
+    }
+
+    function test(value, index, ar) {
+      return index != position - 1;
+    }
+    
+    this.chain  = this.chain.filter(test); 
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;    
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let finish = "";
+    for (let i in this.chain) { 
+      finish += this.start + this.chain[i] + this.end; 
+
+      if (i < (this.chain.length - 1)) {
+        finish += this.link;
+      }
+    }
+    return finish;
   }
 };
 
