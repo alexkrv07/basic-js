d199fd660f57cdc5caab783d8421a7504cd161a9
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-03-22 19:00:50 +0200

    fix: add returns correct depth of nested arrays

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,36 +2,24 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   currentDepth = 0;
-  depth = 0;
+  depth = 1;
 
   calculateDepth(arr) {
-
-    if (!Array.isArray(arr) || arr.length == 0) {
+    if (!Array.isArray(arr)) {
+       this.depth = this.depth > this.currentDepth ? this.depth : this.currentDepth;
+      return this.depth;
+    } else if (arr.length == 0) {
       this.currentDepth++;
-      if (this.currentDepth > this.depth) {
-        this.depth = this.currentDepth;  
+      this.depth = this.depth > this.currentDepth ? this.depth : this.currentDepth;
+      this.currentDepth--;
+      return this.depth;
+    } else {
+      arr.forEach (element => {
+        this.currentDepth++;
+        this.calculateDepth(element);
         this.currentDepth--;
-        return this.depth;
-      }
-      this.currentDepth--
+      });
       return this.depth;
     }
-      arr.forEach(element => { 
-        if (!Array.isArray(element) || element.length == 0) {
-          this.currentDepth++;
-          if (this.currentDepth > this.depth) {
-            this.depth = this.currentDepth;  
-            this.currentDepth--;
-            return this.depth;
-          }
-          this.currentDepth--;
-          return this.depth;
-        }
-        else {
-          this.currentDepth++;
-          return this.calculateDepth(element);
-        }
-      }); 
-      return this.depth;
   }
 };
