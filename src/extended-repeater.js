f9de708251b34c6d10d58d151585e5a31c518f11
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-09-08 22:41:53 +0300

    implement extended repiter

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let repeatTimes = options["repeatTimes"] || 0;
+  let separator = options["separator"] || "+";
+
+  let addition = ("addition" in options) ? options["addition"] + "" : "";
+
+  let additionRepeatTimes = options["additionRepeatTimes"] || 0;
+  let additionSeparator = options["additionSeparator"] || "|";
+
+  let createString = function (str, repeat, separator) {
+    separator = separator + str;
+    for (let i = 0; i < repeat; i++) {
+      if (i > 0) {
+        str = str + separator;
+      }
+    } 
+    return str;
+  } 
+
+  let substring = createString(addition, additionRepeatTimes, additionSeparator);
+  let result = str + substring;
+  return  createString(result, repeatTimes, separator);
 }
