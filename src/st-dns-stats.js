7df77cb738d15f8df84ed1604a58c5a969a47e6b
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-09-10 20:09:07 +0300

    implement getDNSStats

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -22,7 +22,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+export default function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const separator = '.';
+  const result = {};
+  for (let i = 0; i < domains.length; i++) {
+    let nextsearch = domains[i];
+    let index = nextsearch.lastIndexOf(separator);
+    let subdomain = '';
+    while (index >= 0) {
+      subdomain += nextsearch.substring(index);
+      if (result[subdomain]) {
+        result[subdomain]++;
+      } else {
+        result[subdomain] = 1;
+      }
+      nextsearch = nextsearch.substring(0, index);
+      index = nextsearch.lastIndexOf(separator);
+    }
+    subdomain += separator + nextsearch;
+    if (result[subdomain]) {
+      result[subdomain]++;
+    } else {
+      result[subdomain] = 1;
+    }
+  }
+  return result;
 }
