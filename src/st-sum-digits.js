acb1cd69c42d42187dbeed3ac91c0afcf18b7971
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-09-10 21:17:30 +0300

    implement sum-gigits

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -12,7 +12,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
+export default function getSumOfDigits(n) {
+  if (n.toString().length === 1) {
+    return n;
+  }
+  let sum = 0;
+  for (let i = 0; i < n.toString().length; i++) {
+    sum += parseInt(n.toString()[i], 10);
+  }
+  return getSumOfDigits(sum);
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
