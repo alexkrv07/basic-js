2f1155f1bda12ae0119372de969319d961b279a4
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-09-10 21:50:14 +0300

    implement vigener-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,98 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor () {
+    this.direct = true;
+    if (arguments[0] == false) {
+      this.direct = false;
+    }
+  }
+
+  A = "A".charCodeAt(0);
+  Z = "Z".charCodeAt(0);
+  lengthAlphabet = 26;
+
+  encrypt(str, key) {
+
+    if (!str || !key) {
+      throw new Error("Incorrect arguments!")
+    }
+    let shipherText = "";
+    let A = this.A;
+    let Z = this.Z;
+    let lengthAlphabet = this.lengthAlphabet;
+    let j = 0;
+    let direct = this.direct;
+   
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < str.length; i++) {
+      let strIndex = str.charCodeAt(i);
+      let keyIndex = key.charCodeAt(j);
+      if (strIndex < A || strIndex> Z) {
+        shipherText += str[i];
+      }
+      else {
+        let index = (strIndex % A + keyIndex % A) % lengthAlphabet + A;
+        shipherText += String.fromCharCode(index);
+        j++;
+        if (j >= key.length) {
+          j %= key.length;
+        }
+      }
+    }
+    if (direct) {
+      return shipherText;
+    }
+    else {
+      shipherText = shipherText.split("").reverse().join("");
+      return shipherText;
+    }
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(str, key) {
+
+    if (!str || !key) {
+      throw new Error("Incorrect arguments!")
+    }
+    let plainText = "";
+    let A = this.A;
+    let Z = this.Z;
+    let lengthAlphabet = this.lengthAlphabet;
+    let j = 0; 
+    let direct = this.direct;
+
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    
+    for (let i = 0; i < str.length; i++) {
+      let strIndex = str.charCodeAt(i);
+      let keyIndex = key.charCodeAt(j);
+      if (strIndex < A || strIndex> Z) {
+        plainText += str[i];
+      }
+      else {
+        let index;
+        if (strIndex < keyIndex) {
+          index = strIndex + lengthAlphabet - keyIndex + A;
+        }
+        else {
+          index = strIndex - keyIndex + A;
+        }
+        plainText += String.fromCharCode(index);
+        j++;
+        if (j >= key.length) {
+          j %= key.length;
+        }
+      }
+    }
+    if (direct) {
+      return plainText;
+    }
+    return plainText.split("").reverse().join("")
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
