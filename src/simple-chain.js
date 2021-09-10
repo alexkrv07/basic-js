197d9666e3f93584d63f53f3205775bfe427c02b
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-09-10 22:02:15 +0300

    implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,61 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+
+  start: "(",
+  end: ")",
+  link: "~~",
+  chain: [],
+  whitespace: " ",
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length; 
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+
+    value = (!arguments.length) ? this.whitespace : this.whitespace + value + this.whitespace;
+    this.chain.push(value);
+    return this;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+
+    if (!Number.isInteger(position) || position < 0 || position > (this.chain.length - 1)) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+
+    function test(value, index, ar) {
+      return index != position - 1;
+    }
+    
+    this.chain  = this.chain.filter(test); 
+    return this;
+
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.chain.reverse();
+    return this; 
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    let finish = "";
+    for (let i in this.chain) { 
+      finish += this.start + this.chain[i] + this.end; 
+
+      if (i < (this.chain.length - 1)) {
+        finish += this.link;
+      }
+    }
+    this.chain = [];
+    return finish;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 };
